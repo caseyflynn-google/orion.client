bfaea98b59d3ac769f86864a6464e1374b094132
Sidney <xinyij@ca.ibm.com>
2017-06-07 10:54:22 -0400

    load first sharedworkspace if loc is sharedworkspace/tree
    Signed-off-by: Sidney <xinyij@ca.ibm.com>

--- bundles/org.eclipse.orion.client.ui/web/edit/setup.js
+++ bundles/org.eclipse.orion.client.ui/web/edit/setup.js
@@ -1530,8 +1530,7 @@ objects.mixin(EditorSetup.prototype, {
 							return true;
 						}
 					})) {
-						window.location = uriTemplate.expand({resource: workspaces[0].Location});
-//						window.location = uriTemplate.expand({resource: evt.newInput});
+						window.location = uriTemplate.expand({resource: evt.newInput});
 					}
 				});
 			}.bind(this));
